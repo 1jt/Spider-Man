b247917042e01b29afb64a087c3f352d97ef96a7
1jt <2570101165@qq.com>
2024-07-16 13:05:48 +0800

    Add RandomDistribution

---
+++ src/RandomDistribution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomDistribution {
+    private int numPairs; // total number of elements
+    private int maxVolume;
+    ArrayList<Integer> distribution; //
+
+    public RandomDistribution(int numPairs, int maxVolume) {
+        this.numPairs = numPairs;
+        this.maxVolume = maxVolume;
+        this.distribution = new ArrayList<>();
+        GenDistribution();
+    }
+
+    // Method to calculate probabilities for each rank
+    private void GenDistribution() {
+        Random random = new Random();
+        distribution.add(maxVolume);
+        int remainder = numPairs - maxVolume;
+        while(true){
+            int numValues = random.nextInt(maxVolume) + 1;
+            if (remainder < numValues){
+                distribution.add(remainder);
+                break;
+            } else {
+                distribution.add(numValues);
+                remainder -= numValues;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        RandomDistribution rd = new RandomDistribution(1024, 16);
+        // print rd.distribution
+        for (int i = 0; i < rd.distribution.size(); i++) {
+            System.out.print(rd.distribution.get(i) + " ");
+        }
+
+        System.out.println();
+    }
+}
