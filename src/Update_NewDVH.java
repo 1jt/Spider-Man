fa2244e5f0f3036bfb94b88097a88bda0a4f0c9f
z'p <1301036032@qq.com>
2024-07-21 10:30:43 +0800

    修改更新函数的输入，等待功能测试

--- src/Update_NewDVH.java
+++ src/Update_NewDVH.java
@@ -3,88 +3,103 @@ import java.io.*;
 
 public class Update_NewDVH {
 
-    public static ArrayList<NodeSet> Database = new ArrayList<>();
+    public static ArrayList<NodeSet> DeleteUpdate(int size, String key, ArrayList<String> query_result, ArrayList<NodeSet> database) throws IOException {
 
-    public static void getdata(ArrayList<NodeSet> Data){
-        Database = Data ;
-    }
-    public static void DeleteUpdate()throws IOException{
+        //从键盘读取要删除的值
         BufferedReader index = new BufferedReader(new InputStreamReader(System.in));
-        String value;
         String dummy = "Dummy";
         System.out.println("输入要删除的value值");
-        value = index.readLine();
-        ArrayList<Integer>uplist = new ArrayList<>();
-        ArrayList<Integer>list = new ArrayList<>();
-        list = Update_Query_NewDVH.list;
+        String value = index.readLine();
 
-        uplist = UpdateTools.UpList(value,list);
-        int x = uplist.get(0), y = UpdateTest_NewDVH.size - 1 - uplist.get(0);//bound就是size
-        for (int j = 1 ; j < uplist.size() ; j ++){
-            if(uplist.get(j) == 0)
-                y ++;
+        //通过查询获得的查询pos序列，生成更新pos序列
+        ArrayList<Integer> query_list = Update_Query_NewDVH.list;
+        ArrayList<Integer> update_list = NewDVH_Tool.UpList(value, key, query_list, query_result, size);
+
+
+        //找到更新路径对应根节点，通过pos序列计算（x,y）坐标，直接找到目标节点
+        int x = update_list.get(0), y = size - 1 - update_list.get(0);
+        for (int j = 1; j < update_list.size(); j++) {
+            if (update_list.get(j) == 0)
+                y++;
             else x++;
         }
-//        MMPoint Nodemap = new MMPoint(x,y);
 
-        for (NodeSet node : Database) {
+        //在节点库中找到目标节点后，执行删除
+        for (NodeSet node : database) {
             if (node.getNode().getId().getX() == x && node.getNode().getId().getY() == y) {
-                node.getNode().setData(UpdateTest_NewDVH.Query+ "+Value"+ dummy);
+                node.getNode().setData(key + "+Value" + dummy);
             }
         }
+
+        //清空更新序列，为下次更新做准备
         Update_Query_NewDVH.list.clear();
+        return database;
     }
 
-    public static void AddUpdate()throws IOException{
+    public static ArrayList<NodeSet> AddUpdate(int size, String key, ArrayList<String> query_result, ArrayList<NodeSet> database) throws IOException {
+
+        //从键盘读取要添加的值，形式为 Key** + Value**
         BufferedReader index = new BufferedReader(new InputStreamReader(System.in));
-        String value;
         System.out.println("输入要添加的value值");
-        value = index.readLine();
-        String Value = UpdateTest_NewDVH.Query+ "+Value"+ value;
-        ArrayList<Integer>uplist = new ArrayList<>();
-        ArrayList<Integer>list = new ArrayList<>();
-        list = Update_Query_NewDVH.list;
-        uplist = UpdateTools.AddUpList(value,list);
-        if (uplist.size() >= list.size()){
-            int x = uplist.get(0), y = UpdateTest_NewDVH.size - 1 - uplist.get(0);//bound就是size
-            for (int j = 1 ; j < uplist.size()-1 ; j ++){
-                if(uplist.get(j) == 0)
-                    y ++;
+        String value = index.readLine();
+        String data = key + "+Value" + value;//因为更新之前对目标进行过查询，知道key
+
+        //通过查询获得的查询pos序列，生成更新pos序列
+        ArrayList<Integer> query_list = Update_Query_NewDVH.list;
+        ArrayList<Integer> update_list = NewDVH_Tool.AddUpList(value, key, query_list, query_result, size);
+
+        //当更新队列长度>=查询队列长度时，表示不存在dummy数据，添加需要重新创建一个节点
+        if (update_list.size() >= query_list.size()) {
+
+            //使用更新pos序列，计算目标节点父节点(x,y)坐标
+            int x = update_list.get(0), y = size - 1 - update_list.get(0);
+            for (int j = 1; j < update_list.size() - 1; j++) {//这里只计算到其父节点，因为该位置没节点，要初始化一个
+                if (update_list.get(j) == 0)
+                    y++;
                 else x++;
             }
-            for (NodeSet node : Database) {
+
+            //遍历数据库找到父亲节点
+            for (NodeSet node : database) {
                 if (node.getNode().getId().getX() == x && node.getNode().getId().getY() == y) {
-                    TreeNode<String> new_node = new TreeNode<>(Value);
-                    if (uplist.get(uplist.size()-1) == 0){
-                        y ++;
+                    TreeNode<String> new_node = new TreeNode<>(data);
+
+                    //更新序列最后一位表示目标几点和其父亲节点的关系
+                    if (update_list.get(update_list.size() - 1) == 0) {
+                        //左孩子，y++
+                        y++;
                         node.getNode().setLeft(new_node);
-                    }else {
-                        x ++;
+                    } else {
+                        //右孩子，x++
+                        x++;
                         node.getNode().setRight(new_node);
                     }
-                    MMPoint new_point = new MMPoint(x,y);
+
+                    //设置该节点对应的所有数据，把他加入数据库里
+                    MMPoint new_point = new MMPoint(x, y);
                     new_node.setId(new_point);
-                    NodeSet new_nodeset = new NodeSet(new_point,new_node);
-                    Database.add(new_nodeset);
+                    NodeSet new_nodeset = new NodeSet(new_point, new_node);
+                    database.add(new_nodeset);
                     break;
                 }
             }
-        }
-        else {
-            //只需要更改dummy
-            int x = uplist.get(0), y = UpdateTest_NewDVH.size - 1 - uplist.get(0);//bound就是size
-            for (int j = 1 ; j < uplist.size() ; j ++){
-                if(uplist.get(j) == 0)
-                    y ++;
+        } else {//否则，说明路线中有个节点是dummy，将其覆盖即可
+            //使用更新pos序列，计算目标节点(x,y)坐标
+            int x = update_list.get(0), y = size - 1 - update_list.get(0);//
+            for (int j = 1; j < update_list.size(); j++) {
+                if (update_list.get(j) == 0)
+                    y++;
                 else x++;
             }
-            for (NodeSet node : Database) {
+            //找到对应节点，覆盖数据
+            for (NodeSet node : database) {
                 if (node.getNode().getId().getX() == x && node.getNode().getId().getY() == y) {
-                    node.getNode().setData(Value);
+                    node.getNode().setData(data);
                 }
             }
         }
+        //清空更新序列，为下次更新做准备
         Update_Query_NewDVH.list.clear();
-
+        return database;
     }
 }
